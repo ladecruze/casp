01935954809f056d4882eb757f3bb4fa352e0c60
Sivakumar <39010220+ladecruze@users.noreply.github.com>
2022-10-13 11:41:58 +0530

    Update init_db.py

--- csap_api/CASP/init_db.py
+++ csap_api/CASP/init_db.py
@@ -3,7 +3,7 @@ import psycopg2
 
 conn = psycopg2.connect(
         host="casp-db-1",
-        database="postgres",
+        database="casp",
         user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
 
